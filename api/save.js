d3ba8a908874946e47c92ab6b4ecaef5efdfd390
aliifaryadx-host <aliifaryadx@gmail.com>
2026-05-03 01:31:22 +0700

    Add API handler for POST requests in save.js

--- api/save.js
+++ api/save.js
@@ -1,1 +1,9 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    // Ini buat nangkep kiriman data map dari script Delta lu
+    console.log("Data Map Masuk:", req.body);
+    return res.status(200).json({ message: 'Mantap bree, data masuk!' });
+  }
+  res.status(200).send('API Server is Running...');
+}
 
