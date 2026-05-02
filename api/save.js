efc4ec9e83160b797fe35e85a079098346064499
aliifaryadx-host <aliifaryadx@gmail.com>
2026-05-03 01:43:14 +0700

    Implement POST handler in save.js

--- api/save.js
+++ api/save.js
@@ -1,1 +1,8 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log("Data diterima:", req.body);
+    return res.status(200).json({ status: 'Data Masuk, Bree!' });
+  }
+  res.status(200).send('API Ready');
+}
 
